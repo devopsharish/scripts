ecd1068ef1a56d63231f999ea6c71619b5c392be
Trevor Watson <twatson@Trevors-MacBook-Pro.local>
2015-09-21 22:52:20 -0400

    changing logic

--- bash/scanArp.sh
+++ bash/scanArp.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 echo "Scanning Subnet"
-for i in $(arp -a); do echo $i |tr -d '()' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'; done > ips.txt
 
-for i in $(cat ips.txt); do nmap -v $i; done
+function nmapScan(){
+
+cidr=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
+echo Scanning for $cidr/24
+
+nmap -v -F $cidr/24
+
+}
+
+function parseHost(){
+
+echo "Parsing Hosts"
+
+
+echo "Finished Parsing Hosts"
+
+}
+
+nmapScan
+parseHost
+
 
 echo "Done Scanning Subnet"
